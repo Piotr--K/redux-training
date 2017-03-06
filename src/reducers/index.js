68f43f2feae4f0947a3ea7978d4f34d8f099c3b8
Piotr K <pkfilip@hotmail.com>
2017-03-06 21:25:00 +0000

    finishing asynch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,11 @@
 //root reducer
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-  courses //this should be courses: courses but left side is the same as right
-          // so we use synthantic sugar; es6 shorthand property name
-          //name will impact how i access state throut the applicaction. so in
-          //container components we will call state.courses.
+  courses,
+  authors
 });
 
 export default rootReducer;
