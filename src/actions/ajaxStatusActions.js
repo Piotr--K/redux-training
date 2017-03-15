fa13a910abc7aa3a33d0d7a21f04c9f08b2d8452
Piotr K <pkfilip@hotmail.com>
2017-03-15 13:23:36 +0000

    handling async status and providing end user notification

---
+++ src/actions/ajaxStatusActions.js
@@ -0,0 +1,5 @@
+import * as types from './actionTypes';
+
+export function beginAjaxCall() {
+  return {type: types.BEGING_AJAX_CALL};
+}
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 //action creators
 
@@ -20,6 +21,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses(){
   return function(dispatch){
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
@@ -35,6 +37,7 @@ export function loadCourses(){
 //without needing to pass it in
 export function saveCourse(course) {
   return function(dispatch, getState) {
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
